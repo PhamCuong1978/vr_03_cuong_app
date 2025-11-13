bd89cfe7a327e2ef9858bfa8e711cbf96f638076
PhamCuong1978 <cuongpk.tl2@gmail.com>
2025-11-13 23:37:47 +0700

    Fix: base path cho Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-
   const isVercel = process.env.VERCEL === '1';
 
   return {
